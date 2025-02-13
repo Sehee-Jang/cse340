5cabde28c2be57a7a7f2c2d67669581b54ac92a2
Sehee Jang <seheejang94@gmail.com>
2025-02-13 14:55:42 +0900

    add: Week 04 - Learning Activity 1 - Deliver Registration View

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -11,4 +11,15 @@ async function buildLogin(req, res, next) {
   });
 }
 
-module.exports = { buildLogin };
+/* ****************************************
+ *  Build Registeration View
+ * *************************************** */
+async function buildRegister(req, res, next) {
+  let nav = await Util.getNav();
+  res.render("account/register", {
+    title: "Register",
+    nav,
+  });
+}
+
+module.exports = { buildLogin, buildRegister };
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const accountController = require("../controllers/accountController");
 const Util = require("../utilities");
 
-// 로그인 페이지 요청 처리
-router.get("/", Util.handleErrors(accountController.buildLogin));
-// router.get("/login", Util.handleErrors(accountController.buildLogin));
+// Route to build login view
+// router.get("/", Util.handleErrors(accountController.buildLogin));
+router.get("/login", Util.handleErrors(accountController.buildLogin));
+
+// Route to build registration view
+router.get("/register", Util.handleErrors(accountController.buildRegister));
 
 module.exports = router;
