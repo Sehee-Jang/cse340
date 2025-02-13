9d0bdff00a48aa85fec3077f65e1c8b77d83adad
Sehee Jang <seheejang94@gmail.com>
2025-02-13 21:00:43 +0900

    add: Week 04 - Learning Activity 1 - Process the Registration

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,5 @@
 const Util = require("../utilities");
+const accountModel = require("../models/account-model");
 
 /* ****************************************
  *  Build Login View
@@ -22,4 +23,41 @@ async function buildRegister(req, res, next) {
   });
 }
 
-module.exports = { buildLogin, buildRegister };
+/* ****************************************
+ *  Process Registration
+ * *************************************** */
+async function registerAccount(req, res) {
+  let nav = await Util.getNav();
+  const {
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+  } = req.body;
+
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  );
+
+  if (regResult) {
+    req.flash(
+      "notice",
+      `Congratulations, you칟're registered ${account_firstname}. Please log in.`
+    );
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+    });
+  } else {
+    req.flash("notice", "Sorry, the registration failed.");
+    res.status(501).render("account/register", {
+      title: "Registration",
+      nav,
+    });
+  }
+}
+
+module.exports = { buildLogin, buildRegister, registerAccount };
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,4 +10,8 @@ router.get("/login", Util.handleErrors(accountController.buildLogin));
 // Route to build registration view
 router.get("/register", Util.handleErrors(accountController.buildRegister));
 
+// Route to register account
+router.post("/register", Util.handleErrors(accountController.registerAccount));
+
+
 module.exports = router;
