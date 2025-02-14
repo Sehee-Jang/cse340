f6b05a30378cf13f75b348b408fa99a5358fe3c8
Sehee Jang <seheejang94@gmail.com>
2025-02-14 21:15:53 +0900

    add: Week 04 - Team Activity - Email Check and Form Stickiness

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -18,4 +18,11 @@ router.post(
   Util.handleErrors(accountController.registerAccount)
 );
 
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 module.exports = router;
