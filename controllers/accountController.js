2d7e346011dde415357c86037b93eeef76a8c1f2
Sehee Jang <seheejang94@gmail.com>
2025-02-14 21:47:02 +0900

    add: Week 04 - Team Activity - Password Hashing

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,6 @@
 const Util = require("../utilities");
 const accountModel = require("../models/account-model");
-
+const bcrypt = require("bcryptjs");
 /* ****************************************
  *  Build Login View
  * *************************************** */
@@ -36,17 +36,34 @@ async function registerAccount(req, res) {
     account_password,
   } = req.body;
 
+  // Hash the password before storing
+  let hashedPassword;
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10);
+  } catch (error) {
+    req.flash(
+      "notice",
+      "Sorry, there was an error processing the registration."
+    );
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    });
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   );
 
   if (regResult) {
     req.flash(
       "notice",
-      `Congratulations, you칟're registered ${account_firstname}. Please log in.`
+      `Congratulations, you're registered ${account_firstname}. Please log in.`
     );
     res.status(201).render("account/login", {
       title: "Login",
